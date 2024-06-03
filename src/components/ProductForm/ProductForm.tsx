0300aba3ff62ac5212d077ae6401de3e740dfe99
walter lacoste <wlacoste@outlook.com>
2024-06-03 02:03:26 -0300

    refactor: eliminar codigo comentado

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -10,7 +10,7 @@ import Text from '../Text';
 import Collapsible from 'react-native-collapsible';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 
 interface IProductoForm {
   nombre: string;
@@ -53,89 +53,13 @@ const ProductForm = ({ onClose }: IProductForm) => {
 
   const [itemsCategorias, setItemsCategorias] = useState<ListItem[]>([]);
 
-  const [displayValue, setDisplayValue] = useState('');
-
   const [openDate, setOpenDate] = React.useState(false);
 
-  const [product, setProduct] = useState<IProductoForm>({
-    nombre: '',
-    cantidad: undefined,
-    precio: '',
-    isUnitario: false,
-    categoria: '',
-    fechaVencimiento: undefined,
-    isVence: false,
-    fechaCreacion: dayjs().format('DD/MM/YYYY'),
-  });
-
-  const handleChange = (e: string | boolean, nombre: string) => {
-    setProduct(prevProduct => ({
-      ...prevProduct,
-      [nombre]: e,
-    }));
-  };
-  const handleChangeDate = (e: boolean) => {
-    setOpenDate(e);
-
-    setProduct(prevProduct => ({
-      ...prevProduct,
-      fechaVencimiento: e ? dayjs().add(2, 'week').toDate() : undefined,
-      isVence: e,
-    }));
-  };
-
-  const handleInputChange = (text: string, nombre: string) => {
-    const isPositiveInteger = (input: string): boolean => {
-      const parsedValue = parseInt(input, 10);
-      return !isNaN(parsedValue) && parsedValue > 0;
-    };
-    if (text === '' || isPositiveInteger(text)) {
-      const valor = isPositiveInteger(text)
-        ? parseInt(text, 10).toString()
-        : '';
-
-      handleChange(valor, nombre);
-    }
-  };
-
-  const handleCurrencyInput = (value: string) => {
-    const numericValue = value.replace(/\D/g, '');
-    const options = {
-      useGrouping: true,
-      maximumFractionDigits: 0,
-      minimumFractionDigits: 0,
-    };
-    const formattedValue = Number(numericValue)
-      .toLocaleString('en-US', options)
-      .replace(/,/g, '.');
-
-    handleChange(numericValue, 'precio');
-    setDisplayValue(formattedValue);
-    return numericValue;
-  };
-
-  const setFecha = (e: any) => {
-    setProduct(prevProduct => ({
-      ...prevProduct,
-      fechaVencimiento: e,
-    }));
-  };
   useEffect(() => {
     if (categorias) {
       setItemsCategorias(getListItem(categorias));
     }
   }, [categorias]);
-
-  const showDatePicker = () => {
-    DateTimePickerAndroid.open({
-      value: product.fechaVencimiento
-        ? product.fechaVencimiento
-        : dayjs().add(2, 'week').toDate(),
-      onChange: e => setFecha(new Date(e.nativeEvent.timestamp)),
-      mode: 'date',
-      minimumDate: dayjs().toDate(),
-    });
-  };
 
   return (
     <Card style={styles.card}>
@@ -273,7 +197,7 @@ const ProductForm = ({ onClose }: IProductForm) => {
                 value={value}
                 onValueChange={e => {
                   onChange(e);
-                  handleChangeDate(e);
+                  setOpenDate(e);
                   if (!e) {
                     resetField('fechaVencimiento');
                   }
@@ -320,11 +244,7 @@ const ProductForm = ({ onClose }: IProductForm) => {
         </Collapsible>
         <Button
           mode="contained"
-          onPress={
-            // () => console.log('hola')
-            handleSubmit(submit)
-            // onClose?.();
-          }
+          onPress={handleSubmit(submit)}
           style={styles.boton}>
           Console
         </Button>
