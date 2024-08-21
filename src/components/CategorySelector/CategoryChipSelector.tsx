3f17ebd01a639de7c3362b78be973f09a9a21dbe
walter lacoste <wlacoste@andreani.com>
2024-08-21 01:12:09 -0300

    fix: orden categorias

--- src/components/CategorySelector/CategoryChipSelector.tsx
+++ src/components/CategorySelector/CategoryChipSelector.tsx
@@ -1,8 +1,11 @@
 import { View, StyleSheet, ScrollView } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCategories } from '../../context/categoryContext';
 import { Icon, MD3Theme, useTheme } from 'react-native-paper';
 import MaterialChip from 'react-native-material-chip';
+// import { useProductos } from '../../hooks/useProductos';
+import { Categoria } from '../../models/categorias';
+import { useProductContext } from '../../context/productContext';
 
 const getTheme = (seleccionado: boolean, theme: MD3Theme) => {
   if (theme.dark) {
@@ -38,6 +41,18 @@ const CategoryChipSelector = ({
   setSeleccionados,
 }: IChipSelector) => {
   const { categories } = useCategories();
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+  const { productos } = useProductContext();
+  useEffect(() => {
+    if (productos.length === 0) {
+      return;
+    }
+    let cate = [...new Set(productos.map(prod => prod.categoria))].sort(
+      (a, b) => a.ordenCategoria - b.ordenCategoria,
+    );
+
+    setCategorias(cate);
+  }, [productos]);
 
   const theme = useTheme();
 
@@ -51,7 +66,7 @@ const CategoryChipSelector = ({
   return (
     <ScrollView style={styles.container}>
       <View style={styles.chipContainer}>
-        {categories.map((categoria, index) => {
+        {categorias.map((categoria, index) => {
           const seleccionado = seleccionados.includes(categoria.id);
           return (
             // <Chip
@@ -117,6 +132,7 @@ const CategoryChipSelector = ({
 
 const styles = StyleSheet.create({
   container: {
+    minHeight: 90,
     maxHeight: 215, // Adjust this value to control the maximum height of the ScrollView
   },
   chipContainer: {
